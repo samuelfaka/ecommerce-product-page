cad4f4d419254ffe2e0b9178cb527da92832b658
ASUS CORE i5 <samuelfaka@gmail.com>
2024-07-15 00:30:46 -1100

    commer commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function dropup(){
+    const pop = document.querySelector(".puma")
+    pop.style.display = "flex";
+    pop.style.scale = "1.1"
+       const cancel = document.querySelector(".cancel")
+    cancel.style.display = "flex"
+}
+
+function cancel(){
+     pop.style.display = "flex";
+    pop.style.scale = "1.1"
+      pop.cancel = "flex"
+}
---
+++ toggle.js
@@ -0,0 +1,7 @@
+// function dropup(){
+//    const pop = document.querySelector(".puma")
+//    pop.style.dislpay = "flex";
+//    pop.style.scale = "1.0"
+   
+// }
+
